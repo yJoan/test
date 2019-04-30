ee954231d776a6b7ad8b96b54a6b2cd9b46cb955
yJoan <488677134@qq.com>
2019-04-30 20:00:09 +0800

    第二次测试

--- admin/test.cpp
+++ admin/test.cpp
@@ -1,12 +1,53 @@
 #include<iostream>
 #include<Windows.h>
+#include<string>
+
+/*
+int max_val(int a[],int size)
+{
+	int sum = 0;
+	int max = 0;
+	int i=0;
+
+	while (i<size){
+		sum+= a[i];
+		i++;
+			if(sum>max){
+				max=sum;
+
+			}else if(sum<max){
+				sum=0;
+			}
+	}
+	return max;
+}
+*/
+using namespace std;
+
 int main(void)
 {
+	int i;
+	int x;
+	int s=0;
 
-	std::cout<<"你好";
+	for(i=1;i<=100;i++){
+		
+		for(x=1;x<=i;x++){
+			cout<<i<<endl;
+			s++;
+			if(s==100){
+			goto happy;
+		}
+		}
+	
+	}
+
+
+
+happy:
+	cout<<"第"<<s<<"个数是"<<i<<endl;
 
 	system("pause");
-
 	return 0;
 
 }
